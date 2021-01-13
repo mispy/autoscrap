c89d7c5bd758be9472a58d50743609910e63e342
Mispy <mispy@dawnlight.tech>
2021-01-13 17:18:59 +1100

    First step: override to auto disassemble everything

---
+++ Harmony Patches/Patch_XRL_World_Parts_Skill_Tinkering_Disassemble.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using QudUX.Concepts;
+
+namespace QudUX.HarmonyPatches
+{
+    [HarmonyPatch(typeof(XRL.World.Parts.Skill.Tinkering_Disassemble))]
+    class Patch_XRL_World_Parts_Skill_Tinkering_Disassemble
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("WantToDisassemble")]
+        static void Postfix(XRL.World.GameObject __instance, ref bool __result)
+        {
+            __result = true;
+            // if (__result == true && Options.UI.EnableAutogetExclusions)
+            // {
+            //     __result = !XRL.World.Parts.QudUX_AutogetHelper.IsAutogetDisabledByQudUX(__instance);
+            // }
+        }
+    }
+}
